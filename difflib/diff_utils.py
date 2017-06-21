cc74d98eae017343eab496c96919119483150708
刘福云 <920507252@qq.com>
2017-06-21 17:50:42 +0800

    diff utils

--- difflib/diff_utils.py
+++ difflib/diff_utils.py
@@ -22,8 +22,43 @@ def file_mtime(path):
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument('-c', action='store_true', default=False,
+                        help='Produce a context format diff (default)')
+
+    parser.add_argument('-u', action='store_true', default=False,
+                        help='Produce a unified format diff')
+    parser.add_argument('-m', action='store_true', default=False,
+                        help='Produce HTML side by side diff'
+                        '(can use -c and -l in conjunction)')
+    parser.add_argument('-n', action='store_true', default=False,
+                        help='Produce a ndiff format diff')
+    parser.add_argument('-l', '--lines', type=int, default=3,
+                        help='Set number of context lines (default 3)')
+    parser.add_argument('fromfile')
+    parser.add_argument('tofile')
+    options = parser.parse_args()
+
+    n = options.lines
+    fromfile = options.fromfile
+    tofile = options.tofile
+
+    fromdata = file_mtime(fromfile)
+    todata = file_mtime(tofile)
+    with open(fromfile) as ff, open(tofile) as tf:
+        fromlines = ff.readlines()
+        tolines = tf.readlines()
+
+    if options.u:
+        diff = difflib.unified_diff(fromlines, tolines, fromfile, tofile, fromdata, todata, n=n)
+    elif options.n:
+        diff = difflib.ndiff(fromlines, tolines)
+    elif options.m:
+        diff = difflib.HtmlDiff().make_file(fromlines, tolines, fromfile, tofile, context=options.c, numlines=n)
+    else:
+        diff = difflib.context_diff(fromlines, tolines, fromfile, tofile, fromdata, todata, n=n)
+
+    sys.stdout.writelines(diff)
 
 
 if __name__ == '__main__':
-    print(file_mtime(__file__))
-    print(__file__)
+    main()
